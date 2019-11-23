27d29a6fe71a9b692ffb6892f16e1dc766612810
Jonathas Rocha <jonathas.ro@hotmail.com>
2019-11-23 19:03:22 +0000

    Adição da função de leitura dos generos

--- getGenre.py
+++ getGenre.py
@@ -2,6 +2,22 @@ import os
 import tmdbsimple as tmdb
 import csv 
 
+tmdb.API_KEY = os.environ.get('api_key')
+
 def getGenre():
-    genre = tmdb.Genre()
+    
+    genres = tmdb.Genres()
+    genres_list = genres.movie_list()
+    fieldnames = genres_list.get('genres')[0].keys()
+    
+    with open('data/genres.csv', 'w') as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        writer.writeheader()
         
+        print(genres_list)
+        
+        for genre in genres_list.get('genres'):
+            writer.writerow(genre)
+
+if __name__ == '__main__':
+    getGenre()
